be3663ffa167cea61528a040c051bc4cc9e8a0ff
Elouan Jeannot <33838908+elouanj@users.noreply.github.com>
2019-09-08 16:02:57 +0200

    Update server.go

--- server.go
+++ server.go
@@ -4,10 +4,11 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"time"
 )
 
 const (
-	CONN_HOST = "192.168.0.25"
+	CONN_HOST = "192.168.0.11"
 	CONN_PORT = "2019"
 	CONN_TYPE = "tcp"
 )
@@ -41,43 +42,62 @@ func main() {
 // Handles incoming requests.
 func handleRequest(conn net.Conn, users map[net.Addr]net.Conn) {
 	// Make a buffer to hold incoming data.
-	buf := make([]byte, 1)
-	// Read the incoming connection into the buffer.
-	reqLen, err := conn.Read(buf)
-	if err != nil {
-		fmt.Print(reqLen)
-		fmt.Println("Error reading:", err.Error())
-	}
-	// Send a response back to person contacting us.
+	for{
+		buf := make([]byte, 1)
+		// Read the incoming connection into the buffer.
+		reqLen, err := conn.Read(buf)
+		if err != nil {
+			fmt.Print(reqLen)
+			fmt.Println("Error reading:", err.Error())
+		}
+		// Send a response back to person contacting us.
 
 
-	// Close the connection when you're done with it.
+		// Close the connection when you're done with it.
 
-	size := int(buf[0])
-	fmt.Print(size)
-	fmt.Print("Size : " + string(int(size)) + "\n")
+		size := int(buf[0])
+		fmt.Print("Size : ")
+		fmt.Print(size)
 
-	msg_buf := make([]byte, size)
-	// Read the incoming connection into the buffer.
-	length, error := conn.Read(msg_buf)
-	if error != nil {
-		fmt.Print(length)
-		fmt.Println("Error reading:", err.Error())
-	}
-	// Send a response back to person contacting us.
+		msg_buf := make([]byte, size)
+		// Read the incoming connection into the buffer.
+		length, error := conn.Read(msg_buf)
+		if error != nil {
+			fmt.Print(length)
+			fmt.Println("Error reading:", err.Error())
+		}
+		// Send a response back to person contacting us.
 
-	fmt.Print(string(msg_buf) + "\n")
-	if(msg_buf[0] == 109){
-		msg_buf[0] = 32
-		handleMessage(msg_buf,users)
+		fmt.Print(string(msg_buf) + "\n")
+		if(msg_buf[0] == 109){
+			handleMessage(string(msg_buf),users)
+		}
 	}
 
 	//conn.Close()
 }
 
-func handleMessage(message []byte,users map[net.Addr]net.Conn){
+func handleMessage(message string,users map[net.Addr]net.Conn){
 	for k, v := range users {
-		v.Write(message)
-		print("send to " + k.String() + "\n")
+		now := time.Now()
+		sec := now.Format("2006-01-02 15:04:05")
+		text := "i"+string(sec)
+		lenght := byte(len(text))
+		times := []byte(" " + text)
+		times[0] = lenght
+		v.Write(times)
+		println("time : " + string(sec) + "   lenght : " +  string(lenght))
+
+		text = "n"+k.String()
+		lenght = byte(len(text))
+		texte := []byte(" " + text)
+		texte[0] = lenght
+		v.Write(texte)
+		println("message sent to : " + k.String())
+
+		msg := []byte(" " + message)
+		msg[0] = byte(len(message))
+		v.Write(msg)
+		println("message sent : " + string(msg))
 	}
 }
